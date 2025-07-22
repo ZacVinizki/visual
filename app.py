8fee06ac320011149aed73ae7e7ae02bb9d9a4ea
ZacVinizki <Vinizkizac@gmail.com>
2025-07-21 23:11:43 -0400

    Update app.py

--- app.py
+++ app.py
@@ -520,7 +520,7 @@ def create_space_visualization_html(sections: list, company_name: str = "INVESTM
         <div id="subtitle">Investment Thesis Overview</div>
         
         <div id="brain-container">
-            <div id="brain"></div>
+            <div id="brain">🟣</div>
         </div>
     </div>
     
